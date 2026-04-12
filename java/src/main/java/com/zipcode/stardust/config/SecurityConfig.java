a0effcb7ab9bec6c42ad59d0d3d98b8b0de8dd75
nishat <nishatlabiba98@gmail.com>
2026-04-12 14:57:44 -0400

    add @EnableMethodSecurity and moderation routes to controller

--- java/src/main/java/com/zipcode/stardust/config/SecurityConfig.java
+++ java/src/main/java/com/zipcode/stardust/config/SecurityConfig.java
@@ -1,6 +1,5 @@
 package com.zipcode.stardust.config;
 
-import com.zipcode.stardust.repository.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -12,6 +11,8 @@ import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
+
+import com.zipcode.stardust.repository.UserRepository;
 
 @Configuration
 @EnableWebSecurity
